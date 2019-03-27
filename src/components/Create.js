58adf6138fc9c3573cfeca8fba57f46a09361275
Uğur İpekdüzen <u.ipkdzn@gmail.com>
2019-03-28 01:14:43 +0300

    Reactstrap elements added

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react'
-import axios from 'axios';
+import {Form, FormGroup, Button, Input, CardHeader, Card, CardBody, Label} from 'reactstrap'
+import axios from 'axios'
 export default class Create extends Component {
 
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
         kitapAdi: '',
         yazar: '',
         yayinEvi: ''
     }
   }
+
   onChangeKitapAdi = (e) => {
       this.setState({
           kitapAdi: e.target.value
-      });
+      })
   }
 
   onChangeYazar = (e) => {
       this.setState({
           yazar: e.target.value
-      });
+      })
   }
 
   onChangeYayinEvi = (e) => {
     this.setState({
         yayinEvi: e.target.value
-    });
+    })
   }
 
   onSubmit = (e) => {
@@ -36,54 +38,54 @@ export default class Create extends Component {
         yayinEvi: this.state.yayinEvi
       }
       axios.post('https://5c96924f939ad600149a9534.mockapi.io/books', yeniKitap)
-      .then(res => console.log(res.data));
+      .then(res => console.log(res.data))
       
       this.setState({
         kitapAdi: '',
         yazar: '',
         yayinEvi: ''
-      });
+      })
   }
 
   render() {
     return (
-      <div className = "card">
-        <div className = "card-header">
+      <Card>
+        <CardHeader>
             <h4>Kitap Ekle</h4>
-        </div>
-        <div className = "card-body">
-          <form onSubmit = {this.onSubmit}>
-            <div className = "form-group">
-              <label htmlFor = "kitapAdi">Kitap Adı</label>
-              <input type ="text" 
+        </CardHeader>
+        <CardBody>
+          <Form onSubmit = {this.onSubmit}>
+            <FormGroup>
+              <Label htmlFor = "kitapAdi">Kitap Adı</Label>
+              <Input type ="text" 
               name = "kitapAdi"
               placeholder = "Kitap Adı Giriniz"
               className = "form-control"
               onChange = {this.onChangeKitapAdi}
               />
-            </div>
-            <div className = "form-group">
-              <label htmlFor = "yazar">Yazar</label>
-              <input type = "text" 
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor = "yazar">Yazar</Label>
+              <Input type = "text" 
               name = "yazar"
               placeholder = "Yazar Giriniz"
               className = "form-control"
               onChange = {this.onChangeYazar}
               />
-            </div>
-            <div className = "form-group">
-              <label htmlFor = "yayinevi">Yayınevi</label>
-              <input type = "text" 
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor = "yayinevi">Yayınevi</Label>
+              <Input type = "text" 
               name = "yayinevi"
               placeholder = "Yayınevi Giriniz"
               className = "form-control"
               onChange = {this.onChangeYayinEvi}
               />
-            </div>
-            <button className = "btn btn-primary btn-block" type="submit">Ekle</button>
-          </form>
-        </div>
-      </div>
+            </FormGroup>
+            <Button color= "primary" type="submit" block>Ekle</Button>
+          </Form>
+        </CardBody>
+      </Card>
     )
   }
 }
--- src/components/SearchAndList.js
+++ src/components/SearchAndList.js
@@ -1,45 +1,92 @@
 import React, { Component } from 'react'
-import { Table } from 'reactstrap';
+import { Container, Form, FormGroup, Input, Table, Pagination, PaginationItem, PaginationLink, Row } from 'reactstrap';
 import axios from 'axios';
 
 export default class SearchAndList extends Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = { 
             books: [],
-            term: ''
-         };
+            term: '',
+            currentPage: 1,
+            booksPerPage: 10
+         }
 
-        this.searchHandler = this.searchHandler.bind(this);
+        this.handleClick = this.handleClick.bind(this)
+        this.searchHandler = this.searchHandler.bind(this)
       }
+
+      handleClick = (e) => {
+        this.setState({
+          currentPage: Number(e.target.id)
+        })
+      }
+
       searchHandler = (e) => {
-          this.setState({ term: e.target.value });
+          this.setState({ term: e.target.value })
       }
 
       componentDidMount() {
         axios.get('https://5c96924f939ad600149a9534.mockapi.io/books').then((response) => {
           this.setState({ books: response.data })
-        });
-      };
+        })
+      }
       
   render() {
-    const { term, books } = this.state;
-    const filteredData = books.filter(book => {
+    const { term, books, currentPage, booksPerPage } = this.state
+    const indexOfLastBook = currentPage * booksPerPage;
+    const indexOfFirstBook = indexOfLastBook - booksPerPage
+    const currentBooks = books.slice(indexOfFirstBook, indexOfLastBook)
+    
+    const filteredData = currentBooks.filter(book => {
         return book.kitapAdi.toLowerCase().includes(term.toLowerCase()) || 
         book.yazar.toLowerCase().includes(term.toLowerCase()) ||
         book.yayinEvi.toLowerCase().includes(term.toLowerCase() && term !== book.id);
+    }).map(book => {
+      return (
+        <tbody>
+          <tr key= {book.id}>
+            <td>{book.id}</td>
+            <td>{book.kitapAdi}</td>
+            <td>{book.yazar}</td>
+            <td>{book.yayinEvi}</td>
+            <td>
+              <a href={`detail/${book.id}`}>
+                <button className="btn btn-success"> Detaylar</button>
+              </a>
+            </td>
+          </tr>
+        </tbody> 
+      )
     })
+
+    const pageNumbers = []
+      for (let i = 1; i <= Math.ceil(books.length / booksPerPage); i++) {
+        pageNumbers.push(i)
+      }
+
+    const renderPageNumbers = pageNumbers.map(number => {
     return (
-      <div className = "container">
-            <form>
-                <div className="form-group">
-                    <input type="text" 
+      <Pagination size="lg">
+        <PaginationItem key={number}>
+          <PaginationLink  id={number} onClick={this.handleClick}>
+            {number}
+          </PaginationLink>
+        </PaginationItem>
+      </Pagination>
+    )})
+
+    return (
+      <Container>
+            <Form>
+                <FormGroup>
+                    <Input type="text" 
                     onChange={this.searchHandler} 
                     value={term} 
                     className = "form-control"
                     placeholder = "Kitap Adı, Yazar, Yayınevi"/>
-                </div>
-            </form>
+                </FormGroup>
+            </Form>
             <Table>
             <thead>
                 <tr>
@@ -50,25 +97,12 @@ export default class SearchAndList extends Component {
                     <th></th>
                 </tr>
             </thead>
-            <tbody>
-            {
-                filteredData.map(book => 
-                <tr key= {book.id}>
-                    <td>{book.id}</td>
-                    <td>{book.kitapAdi}</td>
-                    <td>{book.yazar}</td>
-                    <td>{book.yayinEvi}</td>
-                    <td>
-                      <a href={`detail/${book.id}`}>
-                        <button className="btn btn-success"> Detaylar</button>
-                      </a>
-                    </td>
-                </tr>
-                ) 
-            }
-            </tbody>
+            {filteredData}
         </Table>
-      </div>
+        <Row>
+        {renderPageNumbers}
+        </Row>
+      </Container>
     )
   }
 }
